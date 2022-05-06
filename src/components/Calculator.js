81434e47eb12eb0e7f4524401b8b60af2d5fb298
Shubh <shubh.scb@gmail.com>
2022-05-06 22:29:30 +0530

    HOOK APPLY SUCCES WITH ERR

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,67 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
-import calculate from '../logic/calculate';
+// import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [result, setResult] = useState('');
+  const handleClicks = (e) => {
+    setResult(result.concat(e.target.value));
+  };
+  const clearDisplay = () => {
+    setResult('');
+  };
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+  const calculate = () => {
+    setResult(eval(result));
+  };
 
-    this.handleClicks = this.handleClicks.bind(this);
-  }
-
-  handleClicks(e) {
-    const Target = e.target.value;
-    this.setState((state) => calculate({
-      next: state.next,
-      total: state.total,
-      operation: state.operation,
-    }, Target));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="container">
-        <div className="display-area">
-          {total}
-          {' '}
-
-          {' '}
-          {operation}
-          {' '}
-
-          {' '}
-          {next}
-          {' '}
-
-        </div>
-        <button onClick={this.handleClicks} type="button" value="AC">AC</button>
-        <button onClick={this.handleClicks} type="button" value="+/-">+/-</button>
-        <button onClick={this.handleClicks} type="button" value="%">%</button>
-        <button onClick={this.handleClicks} type="button" className="orange" value="÷">÷</button>
-        <button onClick={this.handleClicks} type="button" value="7">7</button>
-        <button onClick={this.handleClicks} type="button" value="8">8</button>
-        <button onClick={this.handleClicks} type="button" value="9">9</button>
-        <button onClick={this.handleClicks} type="button" className="orange" value="x">x</button>
-        <button onClick={this.handleClicks} type="button" value="4">4</button>
-        <button onClick={this.handleClicks} type="button" value="5">5</button>
-        <button onClick={this.handleClicks} type="button" value="6">6</button>
-        <button onClick={this.handleClicks} type="button" className="orange" value="-">-</button>
-        <button onClick={this.handleClicks} type="button" value="1">1</button>
-        <button onClick={this.handleClicks} type="button" value="2">2</button>
-        <button onClick={this.handleClicks} type="button" value="3">3</button>
-        <button onClick={this.handleClicks} type="button" className="orange" value="+">+</button>
-        <button onClick={this.handleClicks} type="button" className="large-btn" value="0">0</button>
-        <button onClick={this.handleClicks} type="button" value=".">.</button>
-        <button onClick={this.handleClicks} type="button" className="orange" value="=">=</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <div className="display-area">{result}</div>
+      <button onClick={clearDisplay} type="button" value="AC">AC</button>
+      <button onClick={handleClicks} type="button" value="+/-">+/-</button>
+      <button onClick={handleClicks} type="button" value="%">%</button>
+      <button onClick={handleClicks} type="button" className="orange" value="/">÷</button>
+      <button onClick={handleClicks} type="button" value="7">7</button>
+      <button onClick={handleClicks} type="button" value="8">8</button>
+      <button onClick={handleClicks} type="button" value="9">9</button>
+      <button onClick={handleClicks} type="button" className="orange" value="*">x</button>
+      <button onClick={handleClicks} type="button" value="4">4</button>
+      <button onClick={handleClicks} type="button" value="5">5</button>
+      <button onClick={handleClicks} type="button" value="6">6</button>
+      <button onClick={handleClicks} type="button" className="orange" value="-">-</button>
+      <button onClick={handleClicks} type="button" value="1">1</button>
+      <button onClick={handleClicks} type="button" value="2">2</button>
+      <button onClick={handleClicks} type="button" value="3">3</button>
+      <button onClick={handleClicks} type="button" className="orange" value="+">+</button>
+      <button onClick={handleClicks} type="button" className="large-btn" value="0">0</button>
+      <button onClick={handleClicks} type="button" value=".">.</button>
+      <button onClick={calculate} type="button" className="orange" value="=">=</button>
+    </div>
+  );
+};
 export default Calculator;
+
+// <input type="text" placeholder="0" className="display-area" value={result} />;
+
+// x
+
+// ÷
