498fc87271a4105df751bbaa8a39f1853a026c93
Shubh <shubh.scb@gmail.com>
2022-05-13 07:05:31 +0530

    SERVER JS ADD

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`The server is running at http://localhost:${port}`);
+});
